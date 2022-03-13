4b5a3c2bf0f1f1cd8bbc5227938a5365f6524ef9
Prabowo Murti <prabowo.murti@gmail.com>
2022-03-14 00:01:47 +0700

    feat: fix shuffling and make puzzle solvable

--- rotade/flutter_service_worker.js
+++ rotade/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "edac6610d63f93aebfb1c09a76230248",
-"index.html": "192f0788f3fe35533c62dd79d008b6aa",
-"/": "192f0788f3fe35533c62dd79d008b6aa",
-"main.dart.js": "ce823128d564088810c366ed1675f378",
+"index.html": "beb1040689fd9a56e610abd4bdc2dbaf",
+"/": "beb1040689fd9a56e610abd4bdc2dbaf",
+"main.dart.js": "2d2b52d0958f8fd10d6a09e466f63728",
 "favicon.png": "7a26ea9d4e61122fc023b84e5acdb148",
 "icons/Icon-192.png": "12d05db1c0b26d429b54e3f8c946a496",
 "icons/favicon.png": "7a26ea9d4e61122fc023b84e5acdb148",
